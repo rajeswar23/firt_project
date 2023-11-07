98d0a9254e57a418f933d0d5271618b830fa9848
rajeswar23 <chembetisudheer23@gmail.com>
2023-11-07 21:34:57 +0530

    first commit

---
+++ pencap/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def mowa(request):
+    return HttpResponse("<h1 b>mowa i need pencap </h1")
+def mor(request):
+    return HttpResponse("i will provide that to u")
